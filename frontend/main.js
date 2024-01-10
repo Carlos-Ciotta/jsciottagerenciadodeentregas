3219d1c90a3151423550b59ef61373ee144e58ac
Carlos-Ciotta <carlos.ciotta2506@gmail.com>
2024-01-10 15:41:43 -0300

    10-01-24

--- frontend/main.js
+++ frontend/main.js
@@ -1,40 +1,6 @@
 const url_entregas = 'http://127.0.0.1:3000/entregas'
 const url_veiculos = "http://127.0.0.1:3000/veiculos"
 
-function alerta(){
-  alert("AAA")
-}
-
-function inserirEntrega() {
-  const obj={
-    id_entrega: document.getElementById('id_input').value,
-    nome_cliente: document.getElementById('nome_input').value,
-    telefone: document.getElementById('telefone_input').value,
-    bairro: document.getElementById('cmbOpcoes').value,
-    rua: document.getElementById('rua_input').value,
-    situacao: "Aguardando",
-    data_cadastro: document.getElementById('datePicker').value,
-    hora_cadastro: document.getElementById('radio').value,
-    data_entrega: document.getElementById('datePicker').value,
-    hora_entrega: document.getElementById('radio').value,
-    observacao: document.getElementById('observacao_input').value,
-    vendedor: document.getElementById('radio2').value,
-  }
-
-  axios.post(url_entregas, obj)
-        .then(response => {
-            alert('Objeto inserido com sucesso:', response.data);
-            // Adicione o código que deseja executar após a inserção bem-sucedida
-        })
-        .catch(error => {
-            alert('Erro ao inserir o objeto:', error);
-            // Adicione o código que deseja executar em caso de erro
-        });
-  // Adicione o código que deseja executar aqui
-};
-
-
-
 function resetCampos() {
   document.getElementById("id_input").value = "";
   document.getElementById("nome_input").value = "";
