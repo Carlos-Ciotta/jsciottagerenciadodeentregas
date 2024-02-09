5b703afc11b7db0db08464e6eec769542f46a35a
Carlos-Ciotta <carlos.ciotta2506@gmail.com>
2024-02-09 09:02:49 -0300

    090224

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const entregasRoutes = require('../src/routes/entregas');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/', entregasRoutes); 
-
+mongoose.connect('mongodb+srv://ndstr:<password>@cluster0.pfyukxj.mongodb.net/?retryWrites=true&w=majority')
 app.listen(process.env.PORT || 3000, () => {
 console.log(`Servidor rodando na porta 3000`);});
