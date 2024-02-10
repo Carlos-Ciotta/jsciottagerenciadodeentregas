33802af1f7df96fc3dfad7e17f329217958c6ad9
Carlos-Ciotta <carlos.ciotta2506@gmail.com>
2024-02-10 10:43:40 -0300

    10022024

--- backend/src/controllers/entregas.js
+++ backend/src/controllers/entregas.js
@@ -1,160 +1,5 @@
 const Entrega = require('../models/entregas');
-/*
-module.exports = {
 
-async getAllEntregasFilterUser (req,res, next){
-    const colunas = ['id_entrega', 'nome_cliente', 'bairro', 'situacao','vendedor', 'observacao', 'hora_entrega'];
-    try{
-        const entregas = await Entrega.findAll({attributes: colunas,
-            where: {situacao: ['Aguardando', 'Em andamento']}});
-        if (entregas){
-            res.status(200).json(entregas);
-        }
-        else{
-            const erro = new Error("Entregas não encontradas");
-            erro.status(404);
-            return next(erro);
-        }
-    } catch(error){
-        console.error('Erro ao obter entregas', error);
-    }
-},
-async getAllEntregasFilterOperador (req,res, next){
-    const { situacao, id_veiculo } = req.params;
-    const colunas = ['id_entrega', 'nome_cliente', 'bairro', 'hora_entrega', 'data_entrega', 'observacao','situacao'];
-    try{
-        const entregas = await Entrega.findAll({attributes: colunas,
-            where: {situacao: situacao, id_veiculo: id_veiculo}});
-        if (entregas){
-            res.status(200).json(entregas);
-        }
-        else{
-            const erro = new Error("Entregas não encontradas");
-            erro.status(404);
-            return next(erro);
-        }
-    } catch(error){
-        console.error('Erro ao obter entregas', error);
-    }
-},
-async getAllEntregasFilterOperador1 (req,res, next){
-    const { situacao, id_veiculo } = req.params;
-    const colunas = ['id_entrega', 'nome_cliente', 'bairro'];
-    try{
-        const entregas = await Entrega.findAll({attributes: colunas,
-            where: {situacao: situacao, id_veiculo: id_veiculo}});
-        if (entregas){
-            res.status(200).json(entregas);
-        }
-        else{
-            const erro = new Error("Entregas não encontradas");
-            erro.status(404);
-            return next(erro);
-        }
-    } catch(error){
-        console.error('Erro ao obter entregas', error);
-    }
-},
-async getLeastEntregues (req,res, next){
-    const colunas = ['id_entrega', 'nome_cliente','bairro', 'situacao','vendedor', 'observacao'];
-    try{
-        const entregas = await Entrega.findAll({attributes: colunas,
-            where: {situacao: ['Entregue']},
-            limit: 20});
-        if (entregas){
-            res.status(200).json(entregas);
-        }
-        else{
-            const erro = new Error("Entregas não encontradas");
-            erro.status(404);
-            return next(erro);
-        }
-    } catch(error){
-        console.error('Erro ao obter entregas', error);
-    }
-},
-
-async getEntregaById(req,res, next){
-    const id_entrega = req.params.id_entrega;
-    try{
-        const entrega = await Entrega.findOne({
-            where: {
-              id_entrega: id_entrega,
-            },
-          });
-        if (entrega){
-            res.status(200).json(entrega)
-        }
-        else{
-            const erro = new Error("Entrega não encontrada");
-        }
-    } catch(error){
-        console.error('Erro ao obter entrega', error);
-    }
-},
-
-async createEntrega(req, res){
-    const { id_entrega, id_veiculo, nome_cliente, bairro, situacao, data_entrega, hora_entrega, observacao, vendedor } = req.body;
-    try {
-    const entrega = await Entrega.create({ id_entrega,id_veiculo, nome_cliente, bairro, situacao, data_entrega, hora_entrega, observacao, vendedor });
-    res.status(201).json(entrega);
-    } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erro ao criar entrega.' });
-    }
-},
-
-async deleteEntrega(req, res, next){
-    const id_entrega = req.params.id;
-    try{
-            const entrega = await Entrega.findByPk(id_entrega);
-            if(entrega){
-                Entrega.destroy({ where: { id_entrega } })
-                res.status(200).json({
-                    'status':'Entrega excluida'
-                });
-            }
-            else{
-                const erro = new Error("Entrega não encontrada");
-                erro.status(404);
-                return next(erro);
-            }
-        }catch(error){
-            console.error(error);
-            res.status(500).json({ error: 'Erro ao excluir entrega.' });
-        }
-    },
-async updateEntrega(req, res, next){
-        const { id_entrega } = req.params;
-        try {
-        const [updatedRows] = await Entrega.update(req.body, { where: { id_entrega } });
-        if (updatedRows > 0) {
-        const updatedEntrega = await Entrega.findByPk(id_entrega);
-        res.status(200).json(updatedEntrega);
-        } else {
-        res.status(404).json({ error: 'Entrega não encontrada ou sem alterações.' });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Erro ao atualizar Entrega.' });
-    }
-},
-async updateVeiculoEntrega(req, res, next){
-        const { id_entrega, id_veiculo, situacao } = req.params;
-        try {
-        const [updatedRows] = await Entrega.update({id_veiculo: id_veiculo, situacao: situacao}, { where: { id_entrega } });
-        if (updatedRows > 0) {
-        const updatedEntrega = await Entrega.findByPk(id_entrega);
-        res.status(200).json(updatedEntrega);
-        } else {
-        res.status(404).json({ error: 'Entrega não encontrada ou sem alterações.' });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Erro ao atualizar Entrega.' });
-    }
-    }
-};*/
 module.exports = {
     async getAllEntregasFilterUser(req, res, next) {
         try {
@@ -208,7 +53,7 @@ module.exports = {
 
     async getLeastEntregues(req, res, next) {
         try {
-            const entregas = await Entrega.find({ situacao: 'Entregue' }).limit(20).select('id_entrega nome_cliente bairro situacao vendedor observacao -_id');
+            const entregas = await Entrega.find({ situacao: { $in: 'entregue'}}).limit(20).select('id_entrega nome_cliente bairro situacao vendedor observacao -_id');
             if (entregas.length > 0) {
                 res.status(200).json(entregas);
             } else {
@@ -242,7 +87,9 @@ module.exports = {
     async createEntrega(req, res) {
         const { id_entrega, id_veiculo, nome_cliente, bairro, situacao, data_entrega, hora_entrega, observacao, vendedor } = req.body;
         try {
-            const entrega = await Entrega.create({ id_entrega, id_veiculo, nome_cliente, bairro, situacao, data_entrega, hora_entrega, observacao, vendedor });
+            const entrega = new Entrega ({ id_entrega, id_veiculo, nome_cliente, bairro, situacao, data_entrega, hora_entrega, observacao, vendedor });
+            await entrega.save();
+            /*const entrega = await Entrega.create({ id_entrega, id_veiculo, nome_cliente, bairro, situacao, data_entrega, hora_entrega, observacao, vendedor });*/
             res.status(201).json(entrega);
         } catch (error) {
             console.error(error);
@@ -253,17 +100,8 @@ module.exports = {
     async deleteEntrega(req, res, next) {
         const id_entrega = req.params.id;
         try {
-            const entrega = await Entrega.findById(id_entrega);
-            if (entrega) {
-                await entrega.remove();
-                res.status(200).json({
-                    'status': 'Entrega excluida'
-                });
-            } else {
-                const erro = new Error("Entrega não encontrada");
-                erro.status = 404;
-                return next(erro);
-            }
+            const entrega = await Entrega.findByIdAndDelete(id_entrega);
+            res.send(entrega);
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Erro ao excluir entrega.' });
